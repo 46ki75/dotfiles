7ce6cac31c24f7312720baa895666c3d5d49bfd5
Ikuma Yamashita <me@ikuma.cloud>
2026-09-21 19:32:18 +0900

    feat: enable Neovide native tabs with home-relative titles

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,6 +6,9 @@ vim.opt.timeoutlen = 500
 vim.opt.background = "dark"
 
 if vim.g.neovide then
+  vim.opt.title = true
+  vim.opt.titlestring = "%{fnamemodify(getcwd(), ':~')}"
+
   -- Match Ghostty's size and disable hinting to reduce heavy-looking strokes.
   vim.opt.guifont = "DM Mono Light,Symbols Nerd Font Mono,Zen Kaku Gothic New:h13:#h-none"
   -- Set at startup because Neovide can cache font rendering.
